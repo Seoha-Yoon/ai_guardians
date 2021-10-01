fb5fdfe8365621abf6db893cc0e561924175e246
seoha <seohayn127@gmail.com>
2021-10-01 23:57:19 +0900

    POST REQUEST 구현

---
+++ app/src/main/java/com/example/ai_guardians/DjangoAPI.java
@@ -0,0 +1,16 @@
+package com.example.ai_guardians;
+
+import okhttp3.MultipartBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Field;
+
+public interface DjangoAPI {
+
+    String DJANGO_SITE="http://10.0.2.2:8000";
+
+    @POST("guardians_of_children/video")
+    Call <ResponseBody> uploadFile();
+}
--- app/src/main/java/com/example/ai_guardians/MainActivity.java
+++ app/src/main/java/com/example/ai_guardians/MainActivity.java
@@ -3,15 +3,31 @@ package com.example.ai_guardians;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView iv;
     Button btn_yes, btn_no;
+    ImageView btn_post;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
         iv = findViewById(R.id.iv_video);
         btn_yes = findViewById(R.id.btn_yes);
         btn_no = findViewById(R.id.btn_no);
+        btn_post = findViewById(R.id.btn_post);
+        tv = findViewById(R.id.percentage);
 
         // imageView에 폭력작면 frame 받아오기
         // getImage();
@@ -43,10 +61,66 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        // 임시롤 결과값 받아오는 버튼
+        btn_post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendpost();
+            }
+        });
     }
+
+    private void sendpost(){
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(1, TimeUnit.MINUTES)
+                .readTimeout(5, TimeUnit.MINUTES)
+                .writeTimeout(5, TimeUnit.MINUTES)
+                .build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(DjangoAPI.DJANGO_SITE)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+
+
+        DjangoAPI postApi= retrofit.create(DjangoAPI.class);
+
+        Call<ResponseBody> call = postApi.uploadFile();
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, retrofit2.Response<ResponseBody> response) {
+
+                if (!response.isSuccessful()) {
+                    Log.e("연결이 비정상적 : ", "error code : " + response.code());
+                    return;
+                }
+
+                ResponseBody body = response.body();
+
+                String result = null;
+                try {
+                    result = body.string();
+                    tv.setText(result);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            @Override
+            public void onFailure (Call < ResponseBody > call, Throwable t){
+                Log.d("fail", t.toString());
+            }
+        });
+
+
+    }
+
 
     private void getImage(){
 
     }
-
 }
